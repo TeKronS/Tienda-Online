7fb03f185a4ff229371962d3570284d2e76a9e6e
TeKron <asper.tk93@gmail.com>
2022-04-07 11:04:22 -0400

    added function to serch

--- src/firebase/firebase-querys.js
+++ src/firebase/firebase-querys.js
@@ -5,40 +5,17 @@ import {
   query,
   where,
   getDocs,
-  getDoc
+  getDoc,
 } from "firebase/firestore/lite";
 
-export const querySales = async (palabra) => {
-  // const collectionRef = collection(db, "Ventas");
-  // console.log(firebase.firestore.FieldPath.documentId())
-  // try {
-  //   const querySnapshot = await getDocs(collectionRef);
-  //   // setState(querySnapshot.docs);
-  //   console.log(querySnapshot.docs);
-  // } catch (e) {
-  //   console.error("Error adding document: ", e);
-  //   return null;
-  // }
+export const querySales = async (idList) => {
   const ventaRef = collection(db, "Ventas");
-  let arr = [
-    " L7iG6UOC2yqvCi9KhjFZ",
-    "SZpoUWyik0EoCVOsZ51K",
-    "UNJdR2gLSr0FYqfiTaUV",
-    "Ulet8VNcANCWONDm0Ilv",
-    "XflHvqHBblyjq2PcB8PI",
-    "ZcmIJN9wab2YeMJol4v0",
-    "hrrZBUIu1XJr8O0oOdhY",
-    "oINbgo3yD9dxocugdXZk",
-    "sTa87t9rgCLXFNDR3IZQ",
-    "v25ELDVFfQg5i7sbo5jv",
-    "wOGQUBZfA3EqzyVOxqYT",
-    "y7kQMawB58Alno3mGPUj",
-    "y8o3zK25TjAAWVp8zB1F"
-  ];
-  const q = query(ventaRef, where("__name__", "array-contains-any", arr));
 
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
+  const querySnapshot = await query(ventaRef, where("__name__", "in", idList));
+  console.log(querySnapshot);
+  const b = await getDocs(querySnapshot);
+  console.log(b);
+  b.forEach((doc) => {
     // doc.data() is never undefined for query doc snapshots
     console.log(doc.id, " => ", doc.data());
   });
--- src/header/SearchBarrSection.js
+++ src/header/SearchBarrSection.js
@@ -1,14 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { querySales } from "./../firebase/firebase-querys";
 import { SearchBarrSection } from "./styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { getCollection } from "./../firebase/fire-data-base";
 
 export const SerchBarrComponent = () => {
+  const [titles, setTitles] = useState(null);
   const [query, setQuery] = useState("");
-  function serch() {
+  console.log(titles);
+  useEffect(() => {
+    getCollection("TitlesForSerch").then(async (result) => {
+      const data = await result.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+      setTitles(data);
+    });
+  }, []);
+  // .split(' ')
+  function handledChanged(e) {
+    setQuery(e.target.value);
+  }
+
+  async function serch() {
+    let idList = [];
     if (query) {
-      querySales(query);
+      let keywords = query.split(" ");
+      await keywords.forEach((keyword) => {
+        for (let i = 0, len = titles.length; i < len; i++) {
+          if (titles[i].title.includes(keyword)) {
+            if (!idList.includes(titles[i].id)) {
+              idList.push(titles[i].id);
+            }
+          }
+        }
+      });
+      console.log(idList);
+      if (idList.length > 10) {
+        const newIdList = await breackArray(idList, 10);
+        console.log(newIdList);
+        // querySales(idList);
+      } else {
+        console.log(idList);
+        // querySales(idList);
+      }
+    }
+  }
+
+  function enterPress(e) {
+    if (query) {
+      if (e.key === "Enter") serch();
     }
   }
   return (
@@ -17,9 +58,8 @@ export const SerchBarrComponent = () => {
         value={query}
         type={"text"}
         placeholder={"Busqueda"}
-        // onChange={(e) => {
-        //   setQuery(e.target.value);
-        // }}
+        onChange={handledChanged}
+        onKeyPress={enterPress}
       />
       <button onClick={serch}>
         <FontAwesomeIcon icon={faSearch} />
@@ -27,3 +67,17 @@ export const SerchBarrComponent = () => {
     </SearchBarrSection>
   );
 };
+
+async function breackArray(originalArray, long) {
+  let arregloDeArreglos = []; // Aquí almacenamos los nuevos arreglos
+
+  const LONGITUD_PEDAZOS = long; // Partir en arreglo de 3
+  for (let i = 0; i < originalArray.length; i += LONGITUD_PEDAZOS) {
+    let pedazo = originalArray.slice(i, i + LONGITUD_PEDAZOS);
+    arregloDeArreglos.push(pedazo);
+    if (originalArray.length < i + LONGITUD_PEDAZOS) {
+      return arregloDeArreglos;
+    }
+  }
+  console.log("Arreglo de arreglos: ", arregloDeArreglos);
+}
