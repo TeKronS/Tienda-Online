c286c93134bef94714daa42b7c9123d2ea719264
TeKronS <asper.tk93@gmail.com>
2022-04-17 16:03:56 -0400

    changed text

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,5 +1,14 @@
 import { FooterShop } from "./../Styles/stylesComponents";
 
 export const Footer = () => {
-  return <FooterShop>Pie de Pagina</FooterShop>;
+  return (
+    <FooterShop>
+      Proyecto de Practica{" "}
+      <span>
+        <a href="https://github.com/TeKronS/" target={"blank"}>
+          Simon Ramirez
+        </a>
+      </span>
+    </FooterShop>
+  );
 };
